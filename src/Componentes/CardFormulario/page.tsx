1d96d2e9312f76e50de372da6e6693f1cbb49207
Antonio Nunez <antoniofake1st@gmail.com>
2023-05-01 23:00:25 -0600

    Add hook for for each variable

--- src/Componentes/CardFormulario/page.tsx
+++ src/Componentes/CardFormulario/page.tsx
@@ -1,27 +1,33 @@
 import styles from './styles.module.scss'
 import { UserInterface } from '@/Interfaces/user'
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 
 const Page: React.FC<UserInterface>=({titulo, descripcion, icono}) => {
-    const inputRef = useRef(2);
 
-    const handleClick = () => {
-        alert(inputRef.current.value)
-    }
+   // const [counter, setCounte] = useState(0);
+    const [nombre, setNombre] = useState(0)
+    const [telefono, settelefono] = useState(0)
+    const [email, setemail] = useState(0)
+
 
     return (
         <div className={styles.containerCardTest}>
             <div className={styles.contentCardTest}>
 
                 <p className={styles.headerCard}>{titulo}</p>
-                <input type="text" ref={inputRef}/>
-                <button onClick={handleClick}>enviar</button>
+                    
+                    <input type="name" id="name" placeholder="Introduce tu nombre" required />
+                    
+
                 <p className={styles.cardBody}>{descripcion}</p>
-                <input type="text" ref={inputRef}/>
-                <button onClick={handleClick}>enviar</button>
+                    <input type="telefono" id="telefono" placeholder="Introduce tu telefono" required />
+          
+                
                 <p className={styles.cardBody2}>{icono}</p>
-                <input type="text" ref={inputRef}/>
-                <button onClick={handleClick}>enviar</button>
+                    <input type="email" id="email" placeholder="Introduce tu email" required />
+                    
+                <input type="submit" value="Enviar" class="submit-btn" />
+            
             </div>
         </div>
     )
